79db2694708da65a14127271d00d8c0c4321446a
zhouhao <zhou.hao@99cloud.net>
2022-07-12 15:45:10 +0800

    doc: v2x quick install guide bug fix
    Issue: 138156
    Change-Id: Ic710a2d2b4b9884cc7283e3f0ab885f8ef1bcdf2

--- src/install.sh
+++ src/install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 if [ ! -n "$1" ] ;then
-    echo "you have not input fixip!"
+    echo "you have not input install-environment-ip-adress!"
     exit 1
 fi
+
+unalias cp
+alias docker-compose='docker compose'
 
 mkdir -pv /tmp/pre
 mkdir -pv /tmp/service
@@ -11,16 +14,28 @@ cp -f deploy/docker-compose-pre.yaml /tmp/pre/docker-compose-pre.yaml
 cp -f deploy/docker-compose-service.yaml /tmp/service/docker-compose-service.yaml
 
 sed -i "s/127.0.0.1/$1/" /tmp/service/docker-compose-service.yaml
-docker stop edgeview
-docker rm edgeview
-docker-compose -f /tmp/pre/docker-compose-pre.yaml stop
-docker-compose -f /tmp/pre/docker-compose-pre.yaml rm -f
-docker-compose -f /tmp/service/docker-compose-service.yaml stop
-docker-compose -f /tmp/service/docker-compose-service.yaml rm -f
+
+docker stop redis || true
+docker rm redis || true
+docker stop emqx || true
+docker rm emqx || true
+docker stop mariadb1 || true
+docker rm mariadb1 || true
+docker stop dandelion || true
+docker rm dandelion || true
+docker stop edgeview || true
+docker rm edgeview || true
+docker stop centerview || true
+docker rm centerview || true
+docker stop cerebrum || true
+docker rm cerebrum || true
+docker stop rse-simulator || true
+docker rm rse-simulator || true
 docker rmi openv2x/dandelion:latest || true
-docker rmi openv2x/cereburm:latest || true
+docker rmi openv2x/cerebrum:latest || true
 docker rmi openv2x/edgeview:latest || true
 docker rmi openv2x/centerview:latest || true
+docker rmi openv2x/roadmocker:latest || true
 
 cp -rf deploy/edgeview /etc/
 cp -rf deploy/centerview /etc/
